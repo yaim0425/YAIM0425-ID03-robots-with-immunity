1cf3a1e8959d95cff392c2c5227f5c60a00ac43b
yaim0425 <yaim0425@gmail.com>
2025-08-10 13:25:18 -0500

    Modificación del código

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -20,9 +20,6 @@ function This_MOD.start()
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-    --- Ingredientes a usar
-    This_MOD.build_ingredients()
-
     --- Entidades a afectar
     This_MOD.build_info()
 
@@ -45,23 +42,20 @@ function This_MOD.setting_mod()
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
     --- Información de referencia
-    This_MOD.info = {}
-    This_MOD.ingredients = {}
-    This_MOD.resistances = {}
+    This_MOD.info = { resistances = {} }
 
     --- Referencia
-    This_MOD.types = {}
-    table.insert(This_MOD.types, "construction-robot")
-    table.insert(This_MOD.types, "logistic-robot")
+    This_MOD.types = { "construction-robot", "logistic-robot" }
 
     --- Indicador de mod
     local Indicator = data.raw["virtual-signal"]["signal-heart"].icons[1].icon
 
-    This_MOD.icon = {}
-    This_MOD.icon.tech = { icon = Indicator, scale = 0.50, shift = { 0, -50 } }
-    This_MOD.icon.tech_bg = { icon = GPrefix.color.black, scale = 0.50, shift = { 0, -50 } }
-    This_MOD.icon.other = { icon = Indicator, scale = 0.15, shift = { 0, -12 } }
-    This_MOD.icon.other_bg = { icon = GPrefix.color.black, scale = 0.15, shift = { 0, -12 } }
+    This_MOD.icon = {
+        tech = { icon = Indicator, scale = 0.50, shift = { 0, -50 } },
+        tech_bg = { icon = GPrefix.color.black, scale = 0.50, shift = { 0, -50 } },
+        other = { icon = Indicator, scale = 0.15, shift = { 0, -12 } },
+        other_bg = { icon = GPrefix.color.black, scale = 0.15, shift = { 0, -12 } }
+    }
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 end
@@ -73,76 +67,6 @@ end
 
 
 ---------------------------------------------------------------------------------------------------
-
---- Crear This_MOD.ingredients
-function This_MOD.build_ingredients()
-    --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    --- Lista de ingredientes
-    local Ingredients = {}
-    Ingredients["battery"] = {
-        amount = 3,
-        eval = function(equipment)
-            if equipment.type ~= "battery-equipment" then return end
-            if not equipment.energy_source then return end
-            if not equipment.energy_source.buffer_capacity then return end
-            return GPrefix.number_unit(equipment.energy_source.buffer_capacity)
-        end
-    }
-    Ingredients["solar-panel"] = {
-        amount = 3,
-        eval = function(equipment)
-            if equipment.type ~= "solar-panel-equipment" then return end
-            if not equipment.power then return end
-            return GPrefix.number_unit(equipment.power)
-        end
-    }
-    Ingredients["energy-shield"] = {
-        amount = 3,
-        eval = function(equipment)
-            if equipment.type ~= "energy-shield-equipment" then return end
-            if not equipment.max_shield_value then return end
-            if equipment.max_shield_value == 0 then return end
-            return equipment.max_shield_value
-        end
-    }
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    --- Recorrer los ingredientes
-    for _, ingredient in pairs(Ingredients) do
-        --- Valores de referencia
-        local Now_value = 0
-        local Equipment_name = ""
-
-        --- Buscar el mejor equipo
-        for _, equipment in pairs(GPrefix.equipments) do
-            repeat
-                local New_value = ingredient.eval(equipment)
-                if not New_value then break end
-                if Now_value < New_value then
-                    Equipment_name = equipment.name
-                    Now_value = New_value
-                end
-            until true
-        end
-
-        --- No se encontró equipo
-        if Now_value == 0 then return end
-
-        --- Agregar el muevo ingrediente
-        table.insert(
-            This_MOD.ingredients,
-            {
-                type = "item",
-                name = Equipment_name,
-                amount = ingredient.amount
-            }
-        )
-    end
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-end
 
 --- Información de referencia
 function This_MOD.build_info()
@@ -181,13 +105,7 @@ function This_MOD.build_info()
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
     for damage, _ in pairs(data.raw["damage-type"]) do
-        table.insert(
-            This_MOD.resistances,
-            {
-                type = damage,
-                percent = 100
-            }
-        )
+        table.insert(This_MOD.resistances, { type = damage, percent = 100 })
     end
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -201,11 +119,12 @@ function This_MOD.create_recipe(space)
 
     --- Duplicar la receta
     local Recipe = util.copy(space.recipe)
-    Recipe.main_product = nil
 
     --- Actualizar propiedades
     Recipe.name = GPrefix.delete_prefix(space.recipe.name)
     Recipe.name = This_MOD.prefix .. Recipe.name
+
+    Recipe.main_product = nil
 
     Recipe.icons = util.copy(space.item.icons)
     table.insert(Recipe.icons, This_MOD.icon.other_bg)
@@ -214,24 +133,17 @@ function This_MOD.create_recipe(space)
     local Order = tonumber(Recipe.order) + 1
     Recipe.order = GPrefix.pad_left_zeros(#Recipe.order, Order)
 
-    Recipe.ingredients = util.copy(This_MOD.ingredients)
-    table.insert(
-        Recipe.ingredients,
-        {
-            type = "item",
-            name = space.item.name,
-            amount = 1
-        }
-    )
+    Recipe.ingredients = { {
+        type = "item",
+        name = space.item.name,
+        amount = 1
+    } }
 
     Recipe.results = { {
         type = "item",
         name = This_MOD.prefix .. GPrefix.delete_prefix(space.item.name),
         amount = 1
     } }
-
-    --- Crear la receta
-    GPrefix.extend(Recipe)
 
     --- Agregar a la tecnología
     local Tech = GPrefix.create_tech(This_MOD.prefix, space.tech, Recipe)
