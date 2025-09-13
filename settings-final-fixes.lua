088ca7c386f48656f9ab4309c50d58ec7590f6a4
yaim0425 <yaim0425@gmail.com>
2025-09-13 16:21:50 -0500

    Creación de la opción armor_base

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -50,7 +50,55 @@ table.insert(This_MOD.setting, {
     default_value = 900    --- 15 minutos
 })
 
----------------------------------------------------------------------------
+--- Opcion: armor_base
+table.insert(This_MOD.setting, {
+    type = "string",
+    name = "armor_base",
+    localised_name = { "gui.armor" },
+    default_value = "light-armor",
+    allowed_values = {
+        "light-armor",
+        "heavy-armor",
+        "modular-armor",
+        "power-armor",
+        "power-armor-mk2"
+    }
+})
+
+--- Renombrar
+local Armor_base = This_MOD.setting[#This_MOD.setting]
+
+---------------------------------------------------------------------------------------------------
+
+--- Factorio+
+if mods["factorioplus"] then
+    table.insert(Armor_base.allowed_values, "backpack")
+    table.insert(Armor_base.allowed_values, "backpack-2")
+    table.insert(Armor_base.allowed_values, "explosive-armor")
+    table.insert(Armor_base.allowed_values, "acid-armor")
+end
+
+--- Krastorio 2
+if mods["Krastorio2"] then
+    table.insert(Armor_base.allowed_values, "kr-power-armor-mk3")
+    table.insert(Armor_base.allowed_values, "kr-power-armor-mk4")
+end
+
+--- Youki Industries
+if mods["Yuoki"] then
+    table.insert(Armor_base.allowed_values, "yi_armor_gray")
+    table.insert(Armor_base.allowed_values, "yi_armor_red")
+    table.insert(Armor_base.allowed_values, "yi_armor_gold")
+    table.insert(Armor_base.allowed_values, "yi_walker_a")
+    table.insert(Armor_base.allowed_values, "yi_walker_c")
+end
+
+--- Space Age
+if mods["space-age"] then
+    table.insert(Armor_base.allowed_values, "mech-armor")
+end
+
+---------------------------------------------------------------------------------------------------
 
 
 
