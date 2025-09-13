34100150cf2334b8827aecae00b5e94dd7627a6e
yaim0425 <yaim0425@gmail.com>
2025-09-13 18:12:10 -0500

    Creación e implementación de la función one_resistance

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -192,6 +192,131 @@ end
 
 ---------------------------------------------------------------------------
 
+function This_MOD.create_recipe(space)
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Validación
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    if not space.item then return end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Crear la receta para cada tipo de daño
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    local function one_resistance(damage_type)
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Duplicar el elemento
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        local Recipe = GMOD.copy(This_MOD.recipe)
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Cambiar algunas propiedades
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        Recipe.name = This_MOD.prefix .. damage_type
+
+        Recipe.localised_description = { "" }
+
+        Recipe.localised_name = GMOD.copy(space.item.localised_name)
+        table.insert(Recipe.localised_name, " - ")
+        table.insert(Recipe.localised_name, { "damage-type-name." .. damage })
+
+        Recipe.icons = GMOD.copy(space.item.icons)
+        table.insert(Recipe.icons, This_MOD.indicator)
+
+        Recipe.subgroup = This_MOD.prefix .. This_MOD.name
+
+        Recipe.energy_required = This_MOD.setting.time
+
+        Recipe.results = { {
+            type = "item",
+            name = Recipe.name,
+            amount = 1
+        } }
+
+        Recipe.ingredients = { {
+            type = "item",
+            name = space.item.name,
+            amount = 1
+        } }
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+        --- Crear el prototipo
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        GMOD.extend(Recipe)
+
+        --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    end
+
+    -- local function all_resistance()
+    --     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    --     --- Nueva receta
+    --     local Recipe = GPrefix.copy(This_MOD.recipe)
+    --     local Count = GPrefix.get_length(This_MOD.damages) + 1
+
+    --     --- Actualizar los valores
+    --     Recipe.results[1].name = Recipe.name .. "all"
+    --     Recipe.name = Recipe.name .. "all"
+    --     Recipe.order = GPrefix.pad_left_zeros(This_MOD.digit, Count) .. "0"
+    --     table.insert(Recipe.localised_name, { "gui.all" })
+    --     table.insert(Recipe.icons, This_MOD.icon.other)
+
+    --     --- Agregar los ingredientes
+    --     Recipe.ingredients = {}
+    --     for damage, _ in pairs(This_MOD.damages) do
+    --         table.insert(Recipe.ingredients, {
+    --             type = "item",
+    --             name = This_MOD.recipe.name .. damage,
+    --             amount = 1
+    --         })
+    --     end
+
+    --     --- Agregar la receta
+    --     GPrefix.extend(Recipe)
+
+    --     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    -- end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Crear las recetas
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    for damage, _ in pairs(data.raw["damage-type"]) do
+        one_resistance(damage)
+    end
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+end
+
+---------------------------------------------------------------------------
+
 
 
 
