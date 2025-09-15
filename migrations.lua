75952b616b965cc91e779907bd308d3ba3096c6e
yaim0425 <yaim0425@gmail.com>
2025-09-15 18:02:50 -0500

    Cambio en migrations.lua

--- migrations.lua
+++ migrations.lua
@@ -7,7 +7,7 @@ for _, force in pairs(game.forces) do
     for _, technology in pairs(force.technologies) do
         for _, effect in pairs(technology.prototype.effects) do
             if effect.type == "unlock-recipe" then
-                force.recipes[effect.recipe].enabled = technology.researched or false
+                force.recipes[effect.recipe].enabled = technology.researched
             end
         end
     end
